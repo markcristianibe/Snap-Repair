76b14a61d127866687a27f3d4dac541d8d179454
Mark Cristian Ibe <markcristianibe@gmail.com>
2023-08-01 12:37:53 +0800

    Update mailer.php

--- server/queries/mailer.php
+++ server/queries/mailer.php
@@ -16,8 +16,8 @@ function SendEmail($to, $subject, $body){
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'markcristianibe@gmail.com';            // SMTP username
-        $mail->Password   = 'jnyefkdwyluicizs';                     // SMTP password
+        $mail->Username   = 'repairsnap12@gmail.com';            // SMTP username
+        $mail->Password   = 'ijxiftqgbvkmammy';                     // SMTP password
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
      
